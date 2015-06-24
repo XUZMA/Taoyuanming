4440c1343f8ed207485494fd8ca173c814e1dc90
XUZMA <xuzmnsccsz@yeah.net>
2015-06-24 16:39:17 +0800

    bl p.28

---
+++ bartlett/exit.s
@@ -0,0 +1,33 @@
+# steps:
+# 1. assembler: as exit.s -o exit.o
+# 2. linker: ld exit.o -o exit
+# 3. execute: ./exit
+# 4. echo $?
+
+# PURPOSE:          Simple program that exits and return a
+#                   status code back to the Linux kernel
+#
+# INPUT:            none
+#
+# OUTPUT:   returns a status code. This can be viewed 
+#                   by typing
+#                       echo $?
+#                   after running the program
+#
+# VARIABLES:
+#       %eax holds the system call number
+#       %ebx holds the return status
+#
+.section .data
+
+.section .text
+
+.globl _start
+_start:
+    movl $1, %eax
+# this is the linux kernel command number (system call) for exiting a program
+    movl $0, %ebx
+# this is the status number we will return to the operating system.
+# Change this around and it will return different things to echo $?
+int $0x80
+# this wakes up the kernel to run the exit command.
